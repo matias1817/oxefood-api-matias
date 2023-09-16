3eaae5c0f71f47a08572ad3b2035222407ff5b7b
aluno <aluno@ifpe>
2023-09-16 10:10:36 -0300

    exclusão de entregador

--- src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorController.java
@@ -8,12 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import br.com.ifpe.oxefood.modelo.entregador.Entregador;
 import br.com.ifpe.oxefood.modelo.entregador.EntregadorService;
@@ -43,4 +46,19 @@ public class EntregadorController {
 
         return entregadorService.findById(id);
     }
+
+     @PutMapping("/{id}")
+   public ResponseEntity<Entregador> update(@PathVariable("id") Long id, @RequestBody EntregadorRequest request) {
+
+       entregadorService.update(id, request.build());
+       return ResponseEntity.ok().build();
+   }
+
+   @DeleteMapping("/{id}")
+   public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+       entregadorService.delete(id);
+       return ResponseEntity.ok().build();
+   }
+
 }
--- src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/entregador/EntregadorService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
+
 @Service
 public class EntregadorService {
 
@@ -34,4 +35,43 @@ public Entregador findById(Long id) {
 
     return repository.findById(id).get();
 }
+
+
+@Transactional
+   public void update(Long id, Entregador entregadorAlterado) {
+
+      Entregador entregador = repository.findById(id).get();
+      entregador.setAtivo(entregadorAlterado.getAtivo());
+      entregador.setCpf(entregadorAlterado.getCpf());
+      entregador.setDataNascimento(entregadorAlterado.getDataNascimento());
+      entregador.setEnderecoBairro(entregadorAlterado.getEnderecoBairro());
+      entregador.setEnderecoCep(entregadorAlterado.getEnderecoCep());
+	  entregador.setEnderecoCidade(entregadorAlterado.getEnderecoCidade());
+      entregador.setEnderecoComplemento(entregadorAlterado.getEnderecoComplemento());
+      entregador.setEnderecoNumero(entregadorAlterado.getEnderecoNumero());
+      entregador.setEnderecoRua(entregadorAlterado.getEnderecoRua());
+      entregador.setEnderecoUf(entregadorAlterado.getEnderecoUf());
+	  entregador.setFoneCelular(entregadorAlterado.getFoneCelular());
+      entregador.setFoneFixo(entregadorAlterado.getFoneFixo());
+      entregador.setNome(entregadorAlterado.getNome());
+      entregador.setQtdEntregasRealizadas(entregadorAlterado.getQtdEntregasRealizadas());
+      entregador.setRg(entregadorAlterado.getRg());
+      entregador.setValorFrete(entregadorAlterado.getValorFrete());
+   
+	   
+      entregador.setVersao(entregador.getVersao() + 1);
+      repository.save(entregador);
+  }
+  
+  @Transactional
+  public void delete(Long id) {
+
+      Entregador entregador = repository.findById(id).get();
+      entregador.setHabilitado(Boolean.FALSE);
+      entregador.setVersao(entregador.getVersao() + 1);
+
+      repository.save(entregador);
+  }
+
+
 }
