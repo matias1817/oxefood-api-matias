d9fcd568c8868158a1bb8aa80df9494519b2e09a
aluno <aluno@ifpe>
2023-09-20 11:19:07 -0300

    swagger

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -20,6 +20,11 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.ifpe.oxefood.modelo.cliente.Cliente;
 import br.com.ifpe.oxefood.modelo.cliente.ClienteService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+
 @RestController
 @RequestMapping("/api/cliente")
 @CrossOrigin
@@ -28,25 +33,34 @@ public class ClienteController {
    @Autowired
    private ClienteService clienteService;
 
+   @ApiOperation(value = "Serviço responsável por salvar um cliente no sistema.")
    @PostMapping
    public ResponseEntity<Cliente> save(@RequestBody @Valid ClienteRequest request) {
-
        Cliente cliente = clienteService.save(request.build());
        return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
    }
-
-     @GetMapping
-    public List<Cliente> findAll() {
   
-        return clienteService.findAll();
-    }
+   @ApiOperation(value = "Serviço responsável por listar todos os clientes do sistema.")
+   @GetMapping
+   public List<Cliente> listarTodos() {
+       return clienteService.findAll();
+   }
 
-    @GetMapping("/{id}")
-    public Cliente findById(@PathVariable Long id) {
+   @ApiOperation(value = "Serviço responsável por obter um cliente referente ao Id passado na URL.")
+   @ApiResponses(value = {
+       @ApiResponse(code = 200, message = "Retorna  o cliente."),
+       @ApiResponse(code = 401, message = "Acesso não autorizado."),
+       @ApiResponse(code = 403, message = "Você não tem permissão para acessar este recurso."),
+       @ApiResponse(code = 404, message = "Não foi encontrado um registro para o Id informado."),
+       @ApiResponse(code = 500, message = "Foi gerado um erro no servidor."),
+   })
+   @GetMapping("/{id}")
+   public Cliente obterPorID(@PathVariable Long id) {
+       return clienteService.findById(id);
+   }
 
-        return clienteService.findById(id);
-    }
 
+@ApiOperation(value = "Serviço responsável por alterar um cliente referente ao Id passado na URL.")
     @PutMapping("/{id}")
    public ResponseEntity<Cliente> update(@PathVariable("id") Long id, @RequestBody ClienteRequest request) {
 
@@ -54,6 +68,7 @@ public class ClienteController {
        return ResponseEntity.ok().build();
    }
 
+   @ApiOperation(value = "Serviço responsável por deletar um cliente referente ao Id passado na URL.")
    @DeleteMapping("/{id}")
    public ResponseEntity<Void> delete(@PathVariable Long id) {
 
